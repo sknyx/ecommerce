ed34974c059b01fcfdb3c9c3773ddb10f8ca53a7
Carol Ataide <carol810.x@gmail.com>
2018-05-14 14:58:26 -0300

    Site Categorias

--- index.php
+++ index.php
@@ -307,4 +307,16 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 	exit;
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+	$page->setTpl("category", array(
+		"category"=>$category->getValues(),
+		"products"=>[]));
+
+});
+
 $app->run();
